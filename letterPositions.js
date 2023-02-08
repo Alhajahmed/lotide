2c86dce2408a13bc283fe47f6d14a1e20239d19d
Ahmed <ahmed91alhaj@gmail.com>
2023-02-08 14:52:35 -0500

    Testing

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+const test = letterPositions("hello");
+const expected = {
+  h: [0],
+  e: [1],
+  l: [2, 3],
+  o: [4],
+};
+
+assertArraysEqual(test.h, expected.h);
+assertArraysEqual(test.e, expected.e);
+assertArraysEqual(test.l, expected.l);
+assertArraysEqual(test.o, expected.o);
