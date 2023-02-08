abdb5a5ad06ae067c761ca8a83593a6677c5384b
Ahmed <ahmed91alhaj@gmail.com>
2023-02-08 15:48:47 -0500

    implemented assert Object Equal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,36 @@
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
+  } else {
+    console.log(
+      `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
+  }
+};
+
+const eqObjects = function (object1, object2) {
+  let object1Keys = Object.keys(object1);
+  let object2Keys = Object.keys(object2);
+
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+
+  for (let key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
